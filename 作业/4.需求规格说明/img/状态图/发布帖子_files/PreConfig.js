528df41d11aa679304e0b5bdd82a8191e061f44e
10702 <1070298513@qq.com>
2022-01-29 19:08:37 +0800

    .

---
+++ 作业/4.需求规格说明/img/状态图/发布帖子_files/PreConfig.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2006-2020, JGraph Ltd
+ * Copyright (c) 2006-2020, iodraw.com AG
+ */
+// Overrides of global vars need to be pre-loaded
+window.EXPORT_URL = 'REPLACE_WITH_YOUR_IMAGE_SERVER';
+window.PLANT_URL = 'REPLACE_WITH_YOUR_PLANTUML_SERVER';
+window.IODRAW_BASE_URL = null; // Replace with path to base of deployment, e.g. https://www.example.com/folder
+window.IODRAW_VIEWER_URL = null; // Replace your path to the viewer js, e.g. https://www.example.com/js/viewer.min.js
+window.DRAW_MATH_URL = 'math';
+window.IODRAW_CONFIG = null; // Replace with your custom iodraw.com configurations. For more details, https://desk.iodraw.com/support/solutions/articles/16000058316
+urlParams['sync'] = 'none' // Disabled realtime
---
+++ 作业/README.md
@@ -0,0 +1,12 @@
+# Requirement
+
+组长：zs
+
+组员：
+
+gxy
+
+yxr
+
+fyl
+
